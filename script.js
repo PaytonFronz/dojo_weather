44420c62c5e0f255e79608a3f195116f9bf27465
Payton Fronczak <85954095+PaytonFronz@users.noreply.github.com>
2021-07-27 18:29:25 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var cookiesbanner = document.querySelector(".cookiesbanner");
+
+function hide(.cookiesbanner) {
+    cookiesbanner.classList.add("hide");
+}
+
+
+
+// temp changes???
+var temphigh = document.querySelector(".temphigh")
+function convTemp() {
+    temphigh.innerHTML = "75°";
+}
+
+var temphigh = document.querySelector("temphigh");
+temphigh.innerText = "New Title";
